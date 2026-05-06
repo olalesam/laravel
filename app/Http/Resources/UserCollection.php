8ca5337b171ba49ad3e9064dd9c2bde600213cef
maguettegaye599 <maguettegaye599@gmail.com>
2026-05-06 12:17:47 +0000

    ajout shouwMax

--- app/Http/Resources/UserCollection.php
+++ app/Http/Resources/UserCollection.php
@@ -16,4 +16,9 @@ class UserCollection extends ResourceCollection
     {
         return parent::toArray($request);
     }
+     public function showMax()
+    {
+        return "helo magatte gaye";
+        
+    }
 }
